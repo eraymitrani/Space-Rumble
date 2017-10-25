1ee7b58c606899ac55e53f8063bdd5113c2b143e
ZachNo <zsnokirby@gmail.com>
2017-10-25 17:04:45 -0400

    Added Main Menu and per-player controller setup

---
+++ Assets/Scripts/PlayerControllers.cs
@@ -0,0 +1,54 @@
+﻿using InControl;
+
+public static class PlayerControllers {
+
+    private static InputDevice player1 = null, player2 = null, player3 = null, player4 = null;
+
+    public static InputDevice Player1
+    {
+        get
+        {
+            return player1;
+        }
+        set
+        {
+            player1 = value;
+        }
+    }
+
+    public static InputDevice Player2
+    {
+        get
+        {
+            return player2;
+        }
+        set
+        {
+            player2 = value;
+        }
+    }
+
+    public static InputDevice Player3
+    {
+        get
+        {
+            return player3;
+        }
+        set
+        {
+            player3 = value;
+        }
+    }
+
+    public static InputDevice Player4
+    {
+        get
+        {
+            return player4;
+        }
+        set
+        {
+            player4 = value;
+        }
+    }
+}
---
+++ Assets/Scripts/TrackSelection.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using InControl;
+
+public class TrackSelection : MonoBehaviour {
+
+    public GameObject TrackList;
+    public GameObject TrackSelector;
+    public GameObject TrackPreview;
+    public GameObject TrackTextPrefab;
+
+    int index = 0;
+    int totalTracks = 0;
+    bool moved = false;
+    RectTransform selectorTransform;
+
+    string[] MasterTrackList = new string[]
+    {
+        "Zach's Scene"
+    };
+
+    void Start()
+    {
+        foreach(string s in MasterTrackList)
+        {
+            GameObject track = Instantiate(TrackTextPrefab, TrackList.transform);
+            track.GetComponent<Text>().text = s;
+            totalTracks++;
+        }
+        TrackList.GetComponent<RectTransform>().sizeDelta = new Vector2(0, totalTracks * 100);
+        selectorTransform = TrackSelector.GetComponent<RectTransform>();
+    }
+
+    void Update()
+    {
+        if (InputManager.ActiveDevice.Action1.WasPressed)
+        {
+            SceneManager.LoadScene(index + 1);
+        }
+
+        if (Mathf.Abs(InputManager.ActiveDevice.LeftStickY) < 0.01f)
+            moved = false;
+
+        if (Mathf.Abs(InputManager.ActiveDevice.LeftStickY) > 0.01f && !moved)
+        {
+            if (InputManager.ActiveDevice.LeftStickY < 0)
+            {
+                if (index != totalTracks - 1)
+                {
+                    index += 1;
+                    selectorTransform.localPosition = new Vector3(selectorTransform.localPosition.x, index * -100 - 50, 0);
+                    moved = true;
+                }
+            }
+            else
+            {
+                if (index != 0)
+                {
+                    index -= 1;
+                    selectorTransform.localPosition = new Vector3(selectorTransform.localPosition.x, index * -100 - 50, 0);
+                    moved = true;
+                }
+            }
+        }
+    }
+}
