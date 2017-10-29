09803e414443043a49cedee7cb58d84c32581764
Eray Mitrani <eraymitrani@gmail.com>
2017-10-29 14:31:18 -0400

    added fire

---
+++ Assets/FireAnimation.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireAnimation : MonoBehaviour
+{
+
+    public Sprite[] sprites;
+    private SpriteRenderer sr;
+    public float animationDelay = 0.5f;
+
+	// Use this for initialization
+    //void Awake()
+    //{
+    //    sprites = Resources.LoadAll<Sprite>("Resources/Sprites/fire");
+    //    Debug.Log(sprites.Length);
+
+    //}
+    void Start ()
+	{
+	    sr = GetComponent<SpriteRenderer>();
+	    StartCoroutine(Animate());
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    IEnumerator Animate()
+    {
+        while (true)
+        {
+            sr.sprite = sprites[0];
+            yield return new WaitForSeconds(animationDelay);
+            sr.sprite = sprites[1];
+            yield return new WaitForSeconds(animationDelay);
+            sr.sprite = sprites[2];
+            yield return new WaitForSeconds(animationDelay);
+            sr.sprite = sprites[3];
+            yield return new WaitForSeconds(animationDelay);
+            sr.sprite = sprites[4];
+            yield return new WaitForSeconds(animationDelay);
+            sr.sprite = sprites[5];
+            yield return new WaitForSeconds(animationDelay);
+
+        }
+
+    }
+}
