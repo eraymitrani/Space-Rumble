add5cdb16ed88344d85480770200599733b32106
Eray Mitrani <eraymitrani@gmail.com>
2017-10-30 01:54:42 -0400

    scene updates + progress on weapon

--- Assets/Scripts/WeaponController.cs
+++ Assets/Scripts/WeaponController.cs
@@ -8,7 +8,7 @@ public class WeaponController : MonoBehaviour
     public LayerMask toHitMask;
     private Transform fireLoc;
 
-    public float power = -200f;
+    public float power = 800f;
 	// Use this for initialization
 	void Awake ()
 	{
@@ -24,7 +24,7 @@ public class WeaponController : MonoBehaviour
 	void Update () {
 		if (Input.GetKeyDown("joystick 1 button 1") || Input.GetKeyDown("s"))
 	    {
-            Debug.Log("Shoot");
+           // Debug.Log("Shoot");
 	        Shoot();
 	    }
 	}
@@ -32,25 +32,34 @@ public class WeaponController : MonoBehaviour
     void Shoot()
     {
 		//startpos, radius, direction (change this to be direction of leafblower/right stick), max_distance
-		RaycastHit2D[] hits = Physics2D.CircleCastAll (new Vector2 (fireLoc.position.x, fireLoc.position.y), 2f, Vector2.right, 10f); //, toHitMask);
+        Vector2 angle = new Vector2(transform.position.x,transform.position.y);
+        angle.Normalize();
+		RaycastHit2D[] hits = Physics2D.CircleCastAll (new Vector2 (fireLoc.position.x, fireLoc.position.y), 2f,  angle , 10f, toHitMask);
 
         foreach (var hit in hits)
         {
-			//Debug.Log (hit.point);
-			if (hit.collider.gameObject.tag == "wall") {
-				Vector2 dist = new Vector2 (hit.point.x - fireLoc.position.x, hit.point.y - fireLoc.position.y);
-//				Debug.Log (dist.magnitude);
-//				Debug.Log (dist.x);
-				Debug.Log (hit.distance);
-			}
+			//if (hit.collider.gameObject.tag == "wall") {
+			Vector2 dist = new Vector2 (hit.point.x - fireLoc.position.x, hit.point.y - fireLoc.position.y);
+			//	Debug.Log (hit.distance);
+			//}
 
-//            Debug.Log(hit.collider.tag);
-//            Debug.Log(hit.distance);
-//            if (hit.rigidbody != null && hit.distance != 0)
-//            {
-//                Debug.Log(hit.distance);
-//                hit.collider.attachedRigidbody.AddForce(Vector2.one * (power / hit.distance));
-//            }
+            //            Debug.Log(hit.collider.tag);
+            //            Debug.Log(hit.distance);
+            if (hit.rigidbody != null && dist.magnitude > 0)
+            {
+                Debug.Log(hit.point.x);
+                Debug.Log(fireLoc.position.x);
+                if (hit.point.x < fireLoc.position.x )
+                {
+                    hit.collider.attachedRigidbody.AddForce(angle * (power / dist.magnitude));
+
+                }
+                else
+                {
+                    hit.collider.attachedRigidbody.AddForce(-1*angle * (power / dist.magnitude));
+
+                }
+            }
         }
 
     }
