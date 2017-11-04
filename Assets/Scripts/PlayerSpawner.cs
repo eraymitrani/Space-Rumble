0bae4bf84ac79fc57a06f7b9fdd902c5fa7adf5c
ZachNo <zsnokirby@gmail.com>
2017-11-04 14:29:39 -0400

    Changed how color works

--- Assets/Scripts/PlayerSpawner.cs
+++ Assets/Scripts/PlayerSpawner.cs
@@ -20,12 +20,15 @@ public class PlayerSpawner : MonoBehaviour {
             GameObject player = Instantiate(Player, Spawns[i].transform);
             player.GetComponent<UnityStandardAssets._2D.Platformer2DUserControl>().player_num = i + 1;
             player.GetComponent<Inventory>().scoreManager = scoreManager;
-            switch (i)
+            foreach (SpriteRenderer sr in player.GetComponentsInChildren<SpriteRenderer>())
             {
-                case 0: player.GetComponentInChildren<SpriteRenderer>().color = PlayerControllers.Color1; break;
-                case 1: player.GetComponentInChildren<SpriteRenderer>().color = PlayerControllers.Color2; break;
-                case 2: player.GetComponentInChildren<SpriteRenderer>().color = PlayerControllers.Color3; break;
-                case 3: player.GetComponentInChildren<SpriteRenderer>().color = PlayerControllers.Color4; break;
+                switch (i)
+                {
+                    case 0: sr.color = PlayerControllers.Color1; break;
+                    case 1: sr.color = PlayerControllers.Color2; break;
+                    case 2: sr.color = PlayerControllers.Color3; break;
+                    case 3: sr.color = PlayerControllers.Color4; break;
+                }
             }
         }
 	}
