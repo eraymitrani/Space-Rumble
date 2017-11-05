13d2d3d9acda4601c523ff3e0d00f251b6d7bcf5
njmurray <njmurray@umich.edu>
2017-11-05 16:27:38 -0500

    merge

---
+++ Assets/Scripts/MeteorMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeteorMovement : MonoBehaviour {
+
+	public int meteorSpeed = 2;
+
+	private Rigidbody2D rb;
+
+	void Start () {
+		rb = this.GetComponent<Rigidbody2D> ();
+	}
+	
+	void Update () {
+		rb.velocity = (Vector2.right + Vector2.down) * meteorSpeed;
+	}
+
+	void OnCollisionEnter(Collision other) {
+		Debug.Log ("Colliding!");
+		if (other.collider.tag == "ground") {
+			Destroy (this.gameObject);
+		}
+	}
+}
