6bdba76c975d420c073a161bbe473dc7751395dd
njmurray <njmurray@umich.edu>
2017-11-06 00:15:02 -0500

    meteor prefab (cant get camera to shake on impact)

---
+++ Assets/MeteorMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeteorMovement : MonoBehaviour {
+
+	public delegate IEnumerator MeteorCrash();
+	public static MeteorCrash meteorcrash;
+
+	public int meteorSpeed = 2;
+
+	private Rigidbody2D rb;
+	private Vector2 movedir;
+
+	void Start () {
+		rb = this.GetComponent<Rigidbody2D> ();
+
+		if (this.transform.position.x > 0) {
+			movedir = Vector2.left;
+			this.transform.localScale = new Vector2 (-3, 3);
+		} 
+		else {
+			movedir = Vector2.right;
+		}
+
+		meteorcrash += Explode;
+	}
+	
+	void Update () {
+		rb.velocity = (movedir + Vector2.down) * meteorSpeed;
+	}
+
+	void OnCollisionEnter2D(Collision2D other) {
+		if (other.collider.tag == "ground") {
+			meteorcrash ();
+		}
+	}
+
+	IEnumerator Explode() {
+		Debug.Log (meteorcrash);
+		Destroy (this.gameObject);
+		return null;
+	}
+}
---
+++ Assets/SpawnMeteor.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnMeteor : MonoBehaviour {
+
+	public GameObject meteor;
+
+	public int spawnWait = 15;
+
+	void Start () {
+		StartCoroutine (SpawnLoop ());
+	}
+
+	IEnumerator SpawnLoop() {
+		// change to "while !gameOver
+		while (true) {
+
+			GameObject m = GameObject.Instantiate (meteor);
+
+			if (Random.value < 0.5) {
+				m.transform.position = new Vector2 (-10, 10);
+			}
+			else {
+				m.transform.position = new Vector2 (10, 10);
+			}
+
+			// Wait for next spawn
+			yield return new WaitForSeconds (spawnWait);
+		}
+	}
+}
