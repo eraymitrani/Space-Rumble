e8a842683493727bfab2c5c51567c11a62e57f92
Eray Mitrani <eraymitrani1@hotmail.com>
2017-11-12 19:50:10 -0500

    final scene added scripts

---
+++ Assets/Scripts/DisplayScore.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayScore : MonoBehaviour
+{
+    public float t, timeToTop = 3f;
+    private int numStocksLeft = 3;
+    private Vector2 start, destination;
+	// Use this for initialization
+	void Start ()
+	{
+	    start = transform.position;
+        destination = new Vector2(transform.position.x, transform.position.y + numStocksLeft);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    t += Time.deltaTime / timeToTop;
+	    transform.position = Vector3.Lerp(start, destination, t);
+    }
+}
