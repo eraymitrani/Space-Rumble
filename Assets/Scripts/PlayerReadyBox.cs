c6a169cec27839a3e0a3ecd17c61e9485794c964
ZachNo <zsnokirby@gmail.com>
2017-11-25 01:50:06 -0500

    Revamped main menu for player tutorial

---
+++ Assets/Scripts/PlayerReadyBox.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerReadyBox : MonoBehaviour {
+
+    public bool ready = false;
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        Debug.Log("Hit");
+        if(col.gameObject.name.Equals("CratePink"))
+        {
+            ready = true;
+        }
+    }
+}
