bfb368893d6cb948bec4b726f5a0a71c310b37f1
ZachNo <zsnokirby@gmail.com>
2017-11-27 02:58:33 -0500

    Removed old pause, return to main menu

--- Assets/Scripts/GameLoopManager.cs
+++ Assets/Scripts/GameLoopManager.cs
@@ -1,23 +1,6 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.SceneManagement;
-using InControl;
+﻿using UnityEngine;
 
 
 public class GameLoopManager : MonoBehaviour {
-	
-	// Update is called once per frame
-	void Update () {
-
-		if (InputManager.ActiveDevice.GetControl(InputControlType.Back)){
-            Time.timeScale = Time.timeScale == 1 ? 0 : 1;
-		}
-
-		if (InputManager.ActiveDevice.GetControl(InputControlType.Start)) {
-			SceneManager.LoadScene ("MainMenu");
-		}
-
-	}
 
 }
