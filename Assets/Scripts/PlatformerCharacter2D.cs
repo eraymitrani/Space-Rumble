6d8d43378776410a31cfb2a1237d2cf2610ce388
njmurray <njmurray@umich.edu>
2017-12-03 17:31:40 -0500

    fixed player corner collision glitch

--- Assets/Scripts/PlatformerCharacter2D.cs
+++ Assets/Scripts/PlatformerCharacter2D.cs
@@ -14,7 +14,7 @@ namespace UnityStandardAssets._2D
         [SerializeField] private LayerMask m_WhatIsGround;                  // A mask determining what is ground to the character
 
         private Transform m_GroundCheck;    // A position marking where to check if the player is grounded.
-        const float k_GroundedRadius = .2f; // Radius of the overlap circle to determine if grounded
+        const float k_GroundedRadius = .4f; // Radius of the overlap circle to determine if grounded
         private bool m_Grounded;            // Whether or not the player is grounded.
         private Transform m_CeilingCheck;   // A position marking where to check for ceilings
         const float k_CeilingRadius = .01f; // Radius of the overlap circle to determine if the player can stand up
