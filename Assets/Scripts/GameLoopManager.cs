c9e5ba993a0fd9e1ba0222681b24b991361e4bdf
rsterian <rsterian@umich.edu>
2017-12-10 23:55:36 -0500

    Press space to restart

--- Assets/Scripts/GameLoopManager.cs
+++ Assets/Scripts/GameLoopManager.cs
@@ -1,8 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class GameLoopManager : MonoBehaviour {
 	void Start(){
 		Cursor.visible = false;
+	}
+
+	void Update(){
+		if(Input.GetKeyDown(KeyCode.Space)){
+			SceneManager.LoadScene ("MainMenu");
+		}
 	}
 }
